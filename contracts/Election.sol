0a3f2595f512ce4742e065e7b1902cfbc10277ea
Anirudh Kaushik <anirkkau@usc.edu>
2018-06-26 15:45:21 -0400

    implemented addCandidates w/ truffle integration -ak

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -13,12 +13,23 @@ contract Election {
 	mapping(uint => Candidate) public candidates; //if it's public
 								//sol with create a candidates()
 							   //that will allow us to fetch our candidates
-    //Store Candidates Count
+    //Note: When you add a candidate to the mapping, you are changing
+    //the state of the contract and writing it to the blockchain
 
+
+    //Store Candidates Count
+    uint public candidatesCount;  //state variable
+    						//we'll increment each time we add
 
 	//Constructor
 	function Election() public {
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2");
+	}
 
+	function addCandidate(string _name) private {    //private function
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
 	}
 }
 
